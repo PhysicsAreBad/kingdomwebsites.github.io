2375ec32ff99da7b1f600406c57489f77384087d
Jack Gonser <TheKiller65YT@users.noreply.github.com>
2017-11-06 07:36:39 -1000

    Add easter egg

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 $(document).ready(() => {
 	let $kingdomList = $('#kingdom-list');
 	let $kingdomListHead = $('#kingdom-list-header');
+	let $copyright = $('#copyright');
 	$kingdomList.hide();
 	$kingdomListHead.on('click', () => {
 		$kingdomList.fadeToggle();
+	})
+	$copyright.on('click', () => {
+		alert("Hi");
 	})
 })
