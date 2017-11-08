dc45115e2a4517da95763d1a96cd2449e88804b6
Jack Gonser <jackgonser@gmail.com>
2017-11-07 19:24:23 -1000

    Added a new Javascript format for the Weeb Kingdom

--- weeb-kingdom/app.js
+++ weeb-kingdom/app.js
@@ -1,10 +1,48 @@
 $(document).ready(() => {
-	let $pageButton = $('#page-list-button');
-	let $pageList = $('#page-list');
+	var $pageButton = $('#page-list-button');
+	var $pageList = $('#page-list');
+	var $main = $('.main');
+	var $history = $('.history');
+	var $bio = $('.bio');
+	var $laws = $('.laws');
+	var $cbLink = $('#cb-link');
+	var $historyLink = $('#history-link');
+	var $lawsLink = $('#laws-link');
+	var $mainLink = $('#main-link')
 
+	$history.hide();
+	$bio.hide();
+	$laws.hide();
 	$pageList.hide();
 
 	$pageButton.on('click', () => {
 		$pageList.fadeToggle();
 	})
+	$lawsLink.on('click', () => {
+		$pageList.hide();
+		$main.fadeOut();
+		$bio.fadeOut();
+		$history.fadeOut();
+		setTimeout(function(){$laws.fadeIn();}, 408);
+	})
+	$mainLink.on('click', () => {
+		$pageList.hide();
+		$bio.fadeOut();
+		$history.fadeOut();
+		$laws.fadeOut();
+		setTimeout(function(){$main.fadeIn();}, 408);
+	})
+	$historyLink.on('click', () => {
+		$pageList.hide();
+		$main.fadeOut();
+		$bio.fadeOut();
+		$laws.fadeOut();
+		setTimeout(function(){$history.fadeIn();}, 408);
+	})
+	$cbLink.on('click', () => {
+		$pageList.hide();
+		$main.fadeOut();
+		$history.fadeOut();
+		$laws.fadeOut();
+		setTimeout(function(){$bio.fadeIn();}, 408);})
 })
