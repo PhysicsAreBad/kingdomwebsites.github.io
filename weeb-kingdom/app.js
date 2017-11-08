360f2b4ad88ae68ac446fe9594ad5e9aca6a337f
Jack Gonser <jackgonser@gmail.com>
2017-11-07 19:37:40 -1000

    Rotated 180 uhoh image

--- weeb-kingdom/app.js
+++ weeb-kingdom/app.js
@@ -46,3 +46,14 @@ $(document).ready(() => {
 		$laws.fadeOut();
 		setTimeout(function(){$bio.fadeIn();}, 408);})
 })
+$('img').each(function() {
+    var deg = $(this).data('rotate') || 0;
+    var rotate = 'rotate(' + deg + 'deg)';
+    $(this).css({ 
+        '-webkit-transform': rotate,
+        '-moz-transform': rotate,
+        '-o-transform': rotate,
+        '-ms-transform': rotate,
+        'transform': rotate 
+    });
+});
